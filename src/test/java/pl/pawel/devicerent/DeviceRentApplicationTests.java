8c638c7726742f7d52481230e019cd8bc6ff1241
Pawel Urbanik <pawelurbanik94@gmail.com>
2020-01-05 10:34:29 +0100

    Initial commit

---
+++ src/test/java/pl/pawel/devicerent/DeviceRentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawel.devicerent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
